eead2ae5379ef31de4d567530b48d4d5315c3991
Brian <bernaldev3@gmail.com>
2020-01-29 08:53:15 -0500

    added restart review button for when cards review finishes

--- src/app/cardReview/cardReview.component.ts
+++ src/app/cardReview/cardReview.component.ts
@@ -13,19 +13,29 @@ export class CardReviewComponent implements OnInit {
   index: number;
   data: any;
   flip: boolean;
+  reviewDone: boolean;
+
   constructor(private repository: FlashcardRepository, private activatedRoute: ActivatedRoute) {
     this.index = 0;
     this.flip = false;
+    this.reviewDone = false;
   }
 
   flipCard() {
     this.flip = true;
+  }
+  restartReview() {
+    this.index = 0;
+    this.reviewDone = false;
   }
 
   nextCard() {
     if (this.index < this.flashcards.length - 1) {
       this.flip = false;
       this.index += 1;
+    }
+    else {
+      this.reviewDone = true;
     }
   }
   ngOnInit() {
