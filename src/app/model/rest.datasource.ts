c8bdc9aa31ad76673093ac78e27f5d5af0bcb244
Brian <bernaldev3@gmail.com>
2021-02-15 15:00:40 -0500

    Revising code

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -2,11 +2,12 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Flashcard } from "./flashcard.model";
+
 @Injectable()
 export class RestDataSource {
   baseUrl: string;
   constructor(private httpClient: HttpClient) {
-    this.baseUrl = "http://192.168.1.170:8080/";
+    this.baseUrl = "http://localhost:8080/";
   }
   
   //GET SCHEDULED CARDS
