114858b4d8256385ac91a7e15392c88240823c56
Brian <bernaldev3@gmail.com>
2021-05-18 21:40:11 -0400

    updates

--- src/app/add-card/add-card.component.ts
+++ src/app/add-card/add-card.component.ts
@@ -37,7 +37,7 @@ export class AddCardComponent{
   }
   addCard(flashcard: Flashcard) {
     this.repository.addCard(flashcard);
-    setTimeout(() => this.repository.updateCards(),1000);
+    setTimeout(() => this.repository.updateCards(),500);
     this.router.navigateByUrl("/home")
   }
   get jsonCard() {
